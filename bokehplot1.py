0a3e8af9dba5cc17d6154bb953102b55864e2029
kangas93 <43784037+kangas93@users.noreply.github.com>
2020-05-11 11:03:35 +0200

    Update bokehplot1.py

--- bokehplot1.py
+++ bokehplot1.py
@@ -14,7 +14,7 @@ from math import pi
 from os.path import dirname, join
 
 #data_raw = pd.read_csv(join(dirname(__file__),'data','Motor_Vehicle_Collisions_-_Crashes.csv'))
-data_raw = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv')
+data_raw = pd.read_csv('Motor_Vehicle_Collisions_-_Crashes.csv', low_memory=False)
 #print(data_raw.index)
 
 #dataList=[]
